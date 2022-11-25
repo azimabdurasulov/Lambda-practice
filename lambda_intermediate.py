b19a25651aece2311129d42307ea7fbe372f3732
azimabdurasulov <azimjon20050604@gmail.com>
2022-11-25 16:56:38 +0500

    lambda_

--- lambda_intermediate.py
+++ lambda_intermediate.py
@@ -1,11 +1,13 @@
 # create a lambda function that returns the first element of the list 'l' given as an argument, then assign it to variable 'first1'.
-
+first1 = lambda l: l[0]
 # create a lambda function that returns the last element of the list 'l' given as an argument, then assign it to variable 'last1'.
-
+last1 = lambda l: l[-1]
+print(first1([3, 4, 5, 8]))
 # create a lambda function that returns the length of the list 'l' given as an argument, then assign it to variable 'length1'.
-
+length1 = lambda l: len(l)
 # create a lambda function that returns the first element of the tuple 't' given as an argument, then assign it to variable 'first2'.
-
+first2 = lambda t: t[0]
+print(first2((2, 3, 4, 5)))
 # create a lambda function that returns the last element of the tuple 't' given as an argument, then assign it to variable 'last2'.
 
 # create a lambda function that returns the length of the tuple 't' given as an argument, then assign it to variable 'length2'.
