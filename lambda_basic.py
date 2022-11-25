7ccd4baa309eb066d9dc70d49e8dc122a688c607
azimabdurasulov <azimjon20050604@gmail.com>
2022-11-25 16:37:59 +0500

    lambda_basic.py

--- lambda_basic.py
+++ lambda_basic.py
@@ -1,5 +1,7 @@
 # Create a lambda function that adds 5 to argument 'x', then assign it to variable 'add1'.
+y = lambda x: x + 5
 
+print(y(5))
 # Create a lambda function that multiplies 3 to argument 'x', then assign it to variable 'multiply1'.
 
 # Create a lambda function that finds the remainder when the argument 'x' is divided by 2, then assign it to variable 'remainder'.
